a388befeb65998abab48996c8cf238b9305e2416
Moritz Wolf <moritz.wolf@netlight.com>
2021-08-26 12:44:12 +0200

    add githup pages deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/fun-standup-questions/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'fun-standup-questions',
