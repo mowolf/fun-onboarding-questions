ff24e8b43bcdb894cd5cb8c01d1c2853f00cc88f
Moritz Wolf <moritz.wolf@netlight.com>
2021-08-26 13:05:27 +0200

    rename title

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'fun-standup-questions',
+    title: 'Fun Standup Questions',
     htmlAttrs: {
       lang: 'en'
     },
