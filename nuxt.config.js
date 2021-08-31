bdb8a4b91082572c6eeacf086fac8bf5cf8f329b
Moritz Wolf <moritz.wolf@netlight.com>
2021-08-31 15:03:59 +0200

    try to fix favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,13 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/fun-standup-questions/favicon.ico"
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
