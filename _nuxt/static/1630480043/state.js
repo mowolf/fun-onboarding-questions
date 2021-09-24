d3cce25fa4dc547a0e4cc6330dbe9d7886ccfedf
Moritz Wolf <moritz.wolf@netlight.com>
2021-09-24 09:06:59 +0200

    d685cf2

---
+++ _nuxt/static/1630480043/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffun-standup-questions\u002F_nuxt\u002Fstatic\u002F1630480043",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffun-standup-questions\u002F",assetsPath:"\u002Ffun-standup-questions\u002F_nuxt\u002F",cdnURL:a}}}}(null));
